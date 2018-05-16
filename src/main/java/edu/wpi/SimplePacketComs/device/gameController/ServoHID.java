ec23c6229bb35155d4023f08b9c0b726b1b55e6c
Kevin Harrington <mad.hephaestus@gmail.com>
2018-05-16 18:15:01 -0400

    adding servo object

---
+++ src/main/java/edu/wpi/SimplePacketComs/device/gameController/ServoHID.java
@@ -0,0 +1,26 @@
+package edu.wpi.SimplePacketComs.device.gameController;
+
+import edu.wpi.SimplePacketComs.BytePacketType;
+import edu.wpi.SimplePacketComs.PacketType;
+import edu.wpi.SimplePacketComs.phy.HIDSimplePacketComs;
+
+public class ServoHID extends HIDSimplePacketComs {
+	private PacketType gamestate = new BytePacketType(1804, 64);
+	private final byte[] status = new byte[60];
+	private final byte[] data = new byte[20];
+	
+	public ServoHID(int vidIn, int pidIn) {
+		super(vidIn, pidIn);
+		addPollingPacket(gamestate);
+		addEvent(gamestate.idOfCommand, () -> {
+			readBytes(gamestate.idOfCommand, getData());
+			writeBytes(gamestate.idOfCommand, getStatus());
+		});
+	}
+	public byte[] getStatus() {
+		return status;
+	}
+	public byte[] getData() {
+		return data;
+	}
+}
